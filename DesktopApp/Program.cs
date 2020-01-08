0152e1b496ceba110b73d178b141a3f5d8fb9b50
Daniel Müller <daniel.mueller@docufy.de>
2020-01-08 22:14:54 +0100

    Added Main-Method as Central Entry Point

---
+++ DesktopApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace DesktopApp
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            
+            var mainWindow = new MainWindow();
+            app.Run(mainWindow);
+        }
+    }
+}
